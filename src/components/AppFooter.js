c9ee7a9c7f9d989a15a18948a5908595393e1461
umm-e-habiba <umehabiba0305@gmail.com>
2024-07-11 22:02:35 +0500

    mbl footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -4,16 +4,31 @@ import { Link } from 'react-router-dom'
 
 const AppFooter = () => {
   return (
-    <CFooter className="px-4">
-      <div>
+    <CFooter className="px-4 flex justify-center lg:justify-between ">
+      <div className="hidden lg:block">
         <span className="ms-1">All rights reserved &copy; 2024</span>
       </div>
-      <div className="ms-auto">
-        <span className="me-1">Created by: </span>
-        <Link to="https://themksolution.com/" target="_blank" className="font-semibold">
-          MK Solution
-        </Link>
+      <div className="hidden lg:block">
+        <span className="me-1 text-center">
+          Created by:
+          <Link to="https://themksolution.com/" target="_blank" className="font-semibold">
+            MK Solution
+          </Link>
+        </span>
       </div>
+      <center className="flex lg:hidden flex-col items-center">
+        <center>
+          <span>All rights reserved &copy; 2024</span>
+        </center>
+        <center>
+          <span>
+            Created by:
+            <Link to="https://themksolution.com/" target="_blank" className="font-semibold">
+              MK Solution
+            </Link>
+          </span>
+        </center>
+      </center>
     </CFooter>
   )
 }
